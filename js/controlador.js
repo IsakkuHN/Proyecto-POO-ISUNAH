95c836c14b27c1eff692b1af7046ab5a76efe438
IsakkuHN <60827584+IsakkuHN@users.noreply.github.com>
2020-04-01 15:58:26 -0600

    Actualizacion

---
+++ js/controlador.js
@@ -0,0 +1,36 @@
+Empresas = [
+  {
+    nombre: "",
+    pais: "",
+    direccion: "",
+    banner: "",
+    logotipo: "",
+    fb: "",
+    yt: "",
+    inst: "",
+    sucursales: [
+      {
+        longitudLatitud: "",
+        nombreSucursal: "",
+        promociones:[]
+      }
+    ],
+    productos: [
+      {
+        nombreProducto: "",
+        precio: "",
+        descripcion: "",
+        fotografia: "",
+        promocion: [
+          {
+            fechaInicio: "",
+            fechaFinal: "",
+            sucursales: ""
+          }
+        ]
+      }
+    ]
+  }
+];
+
+console.log(Empresas);
